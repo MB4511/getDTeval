e83307a48fb926a614a372393acfd0133e92f228
mb4511 <mb4511@columbia.edu>
2020-08-19 19:42:20 -0700

    lot of stuff

--- getDTeval.Rcheck/00_pkg_src/getDTeval/R/benchmark.getDTeval.R
+++ getDTeval.Rcheck/00_pkg_src/getDTeval/R/benchmark.getDTeval.R
@@ -12,7 +12,7 @@
 #' @import stats
 #' @import utils
 #' @export
-#NULL
+NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("category","seconds","."))
