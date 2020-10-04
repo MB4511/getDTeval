27a168c34eb7ef15cfcf0203412763beaf79dcf9
Mayur Bansal <mayur@sparrowmarketing.com>
2020-10-03 23:05:19 -0700

    adding unit tests and checking for r checks

--- tests/testthat/test-getDteval.R
+++ tests/testthat/test-getDteval.R
@@ -14,7 +14,7 @@ income.group.name <- "Income Group"
 product.name <- "Product"
 
 
-#1 summarize
+#1 summarize dplyr check
 mean.age.name <- 'mean_age'
 
 #avg age name
@@ -31,99 +31,39 @@ test_that('calculate mean age with dplyr', {
 })
 
 
-# age region age
-# mean_age_region_getDTeval <- getDTeval(
-#   the.statement = 'formulaic::snack.dat %>%
-#                                   group_by(get(region.name)) %>%
-#                                   summarize(mean.age.by.region.name = mean(get(age.name)))',
-#   return.as = 'result'
-# )
-#
-# mean_age_region_dplyr <- formulaic::snack.dat %>%
-#   group_by(Region) %>%
-#   summarize(mean.age.by.region.name = mean(get(age.name)))
-#
-# test_that('calculate mean by region dplyr',
-#           {
-#             expect_equal(mean_age_region_dplyr$mean.age.by.region.name, mean_age_region_getDTeval$mean.age.by.region.name)
-#           })
+#2 group by dplyr check
+mean.age.by.region.name = 'mean_age_by_region'
+
+#avg age by region
+avg_age_by_region_dplyr<-formulaic::snack.dat %>% group_by(Region) %>% summarize(mean_age_by_region = mean(get(age.name)))
+
+avg_age_by_region_getDTeval<- getDTeval(the.statement = 'formulaic::snack.dat %>% group_by(Region) %>% summarize(mean_age_by_region = mean(Age))',return.as='result')
 
 
-#2 group by
-# mean.age.by.region.name = 'mean_age_by_region'
+test_that('calculate mean age by region', {
+  expect_equal(avg_age_by_region_dplyr, avg_age_by_region_getDTeval)
+})
+
 #
-#
-#
-#
-# formulaic::snack.dat %>%
-#   group_by(region.name) %>%
-#   summarize(mean.age.by.region.name) = mean(get(age.name))
-#
-#
-#
-#
-# formulaic::snack.dat %>%
-#   group_by(get(region.name)) %>%
-#   summarise(mean.age.by.region.name = mean(get(age.name)))
-#
-# test_that('calculate mean by region', {
-#   expect_equal(
-#     getDTeval(the.statement = 'dat[, .(eval(mean.age.by.region.name) =  mean(get(age.name))), keyby = region.name]
-#                          ', return.as = 'result'),
-#     dat[, .(mean_age_by_region = mean(get(age.name))), keyby = region.name]
-#   )
-# })
-#
-#
-# #3 filter
-# mean.age.by.persona.name = 'mean_age_by_persona'
-#
-# test_that('calculate mean only for selected persona dplyr', {
-#   expect_equal(
-#     getDTeval(
-#       the.statement = "dat %>%
-#                          group_by(Persona) %>%
-#                          summarise(eval(mean.age.by.persona.name) = mean(get(age.name))) %>%
-#                          filter(Persona %in% c('Millenial Muncher', 'Old School Oliver'))",
-#       return.as = 'result'
-#     ),
-#
-#     dat %>%
-#       group_by(Persona) %>%
-#       summarize(mean_age_by_persona = mean(get(age.name))) %>%
-#       filter(Persona %in% c('Millenial Muncher', 'Old School Oliver'))
-#   )
-# })
-#
-# test_that('calculate mean only for selected perosna', {
-#   expect_equal(
-#     getDTeval(the.statement = "dat[get(persona.name) %in% c('Millenial Muncher', 'Old School Oliver'), .(eval(mean.age.by.persona.name) = mean(get(age.name))), keyby = persona.name]", return.as = 'result'),
-#     dat[get(persona.name) %in% c('Millenial Muncher', 'Old School Oliver'), .('mean_age_by_persona' = mean(get(age.name))), keyby = persona.name]
-#   )
-# })
-#
-#
-# #4 mutate
-# age.decade.name = 'age_decades'
-#
-# test_that('create a new column dplyr', {
-#   expect_equal(
-#     getDTeval(
-#       the.statement = "dat %>%
-#                          mutate(eval(age.decade.name) = floor(get(age.name)/10))",
-#       return.as = 'result'
-#     ),
-#     dat %>%
-#       mutate(age_decades = floor(get(age.name) / 10))
-#   )
-# })
-#
-# test_that('create a new column', {
-#   expect_equal(
-#     getDTeval(
-#       'dat[, eval(age.decade.name) := floor(get(age.name)/10)]',
-#       return.as = 'result'
-#     ),
-#     dat[, age_decades := floor(get(age.name) / 10)]
-#   )
-# })
+#3 mean age by persona
+mean.age.by.persona.name = 'mean_age_by_persona'
+
+getdteval_meanagebypersona<-getDTeval(the.statement = "formulaic::snack.dat %>% group_by(Persona) %>% summarize(mean_age_by_persona = mean(Age))", return.as = 'result')
+
+dplyr_meanagebypersona<-formulaic::snack.dat %>% group_by(Persona) %>% summarize(mean_age_by_persona = mean(get(age.name)))
+
+test_that('calculate mean age by persona', {
+ expect_equal(getdteval_meanagebypersona,dplyr_meanagebypersona)
+})
+
+#4 mutate dplyr check
+age.decade.name = 'age_decades'
+
+test_that('create a new column dplyr', {
+ expect_equal(getDTeval(the.statement = "formulaic::snack.dat%>% mutate(age_decades = floor(Age/10))",return.as = 'result'), formulaic::snack.dat %>% mutate(age_decades = floor(get(age.name) / 10)))
+})
+
+#5 mutate data.table check
+test_that('create a new column', {
+ expect_equal(getDTeval('formulaic::snack.dat[, age_decades := floor(Age/10)]', return.as = 'result'), formulaic::snack.dat[, age_decades := floor(get(age.name) / 10)])
+})
